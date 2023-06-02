aea3a48fd0fe37a6fc9c50421ab2709b25d40a4f
uiysda <102615695+uiysda@users.noreply.github.com>
2023-06-02 23:42:22 +0800

    Update ALU_1bit.v

--- ALU_1bit.v
+++ ALU_1bit.v
@@ -1,23 +1,23 @@
 `timescale 1ns/1ns
 
-module ALU_1bit( dataA, dataB, sel, invertB, cin, dataOut, cout ) ;
+module ALU_1bit( Sel, DataA, DataB, InvertB, Cin, DataOut, Cout ) ;
 
-  input dataA, dataB, cin ;
-  input[1:0] sel ;
-  input invertB ;
+  input DataA, DataB, Cin, InvertB ;
+  input[1:0] Sel ;
+  input InvertB ;
 
-  output dataOut, cout;
+  output DataOut, Cout;
 
   wire and00, or01, add10_sub11, tempB ;
 
-  assign and00 = dataA & dataB ;
+  assign and00 = DataA & DataB ;
   
-  assign or01 = dataA | dataB ;
+  assign or01 = DataA | DataB ;
   
-  assign tempB = invertB ^ dataB ;
+  assign tempB = InvertB ^ DataB ;
   
-  FA fullAdder( .DataA( dataA ), .DataB( tempB ), .Cin( cin ), .Sum( add10_sub11 ), .Cout( cout ) ) ;
+  FA fullAdder( .DataA( DataA ), .DataB( tempB ), .Cin( Cin ), .Sum( add10_sub11 ), .Cout( Cout ) ) ;
   
-  MUX_4to1 selectResult( .in1( and00 ), .in2( or01 ), .in3( add10_sub11 ), .in4( add10_sub11 ), .sel( sel ), .dataOut( dataOut ) ) ;
+  MUX_4to1 selectResult( .Sel( sel ), .DataIn0( and00 ), .DataIn1( or01 ), .DataIn2( add10_sub11 ), .DataIn3( add10_sub11 ), .DataOut( dataOut ) ) ;
   
 endmodule
