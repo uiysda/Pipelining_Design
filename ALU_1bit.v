bcd97f4fee49717f1d67d5a145695f11eb3b850e
David25062976 <102165811+David25062976@users.noreply.github.com>
2023-06-02 23:24:43 +0800

    ALU_1bit.v

--- ALU_1bit.v
+++ ALU_1bit.v
@@ -32,3 +32,27 @@ module ALU_1bit( dataA, dataB, Signal, cin, dataOut, cout );
                 (Signal == SUB  || Signal == SLT) ? subCarry : 0;
 
 endmodule
+
+`timescale 1ns/1ns
+
+module ALU_1bit( dataA, dataB, sel, invertB, cin, dataOut, cout ) ;
+
+  input dataA, dataB, cin ;
+  input[1:0] sel ;
+  input invertB ;
+
+  output dataOut, cout;
+
+  wire and00, or01, add10_sub11, tempB ;
+
+  assign and00 = dataA & dataB ;
+  
+  assign or01 = dataA | dataB ;
+  
+  assign tempB = invertB ^ dataB ;
+  
+  FA fullAdder( .DataA( dataA ), .DataB( tempB ), .Cin( cin ), .Sum( add10_sub11 ), .Cout( cout ) ) ;
+  
+  MUX_4to1 selectResult( .in1( and00 ), .in2( or01 ), .in3( add10_sub11 ), .in4( add10_sub11 ), .sel( sel ), .dataOut( dataOut ) ) ;
+  
+endmodule
