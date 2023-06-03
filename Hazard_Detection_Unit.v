1b3e55ec516659a191f918277d5ea25362c45480
uiysda <102615695+uiysda@users.noreply.github.com>
2023-06-03 23:49:23 +0800

    Update Hazard_Detection_Unit.v

--- Hazard_Detection_Unit.v
+++ Hazard_Detection_Unit.v
@@ -1,8 +1,8 @@
 `timescale 1ns/1ns
 
-module Hazard_Detection_Unit( MemRead_EX, Rs_ID, Rt_ID, Rt_EX, Stall );
+module Hazard_Detection_Unit( Clk, MemRead_EX, Rs_ID, Rt_ID, Rt_EX, Stall );
 
-  input       MemRead_EX;
+  input       Clk, MemRead_EX;
   input [4:0] Rs_ID, Rt_ID, Rt_EX;
 
   output      Stall ;
