aadf8d5862e3295c9147dba7faa0df749a9bbda9
uiysda <102615695+uiysda@users.noreply.github.com>
2023-06-03 23:48:42 +0800

    Update Hazard_Detection_Unit.v

--- Hazard_Detection_Unit.v
+++ Hazard_Detection_Unit.v
@@ -2,11 +2,19 @@
 
 module Hazard_Detection_Unit( MemRead_EX, Rs_ID, Rt_ID, Rt_EX, Stall );
 
-    input       MemRead_EX;
-    input [4:0] Rs_ID, Rt_ID, Rt_EX;
+  input       MemRead_EX;
+  input [4:0] Rs_ID, Rt_ID, Rt_EX;
 
-    output      Stall ;
-
-    assign DataOut = ( MemRead_EX && ( ( Rs_ID == Rt_EX ) || ( Rt_ID == Rt_EX ) ) ) ? 1'b1 : 1'b0;
-
+  output      Stall ;
+    
+  always@( posedge Clk )
+  begin  
+      
+    if ( MemRead_EX && ( ( Rs_ID == Rt_EX ) || ( Rt_ID == Rt_EX ) ) )
+      Stall = 1b'1;
+    else
+      Stall = 1b'0;
+      
+  end
+    
 endmodule
