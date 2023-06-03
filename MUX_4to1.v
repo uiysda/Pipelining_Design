3279c70dbc3b2d2700c08ab63f7962b935c1cdb4
uiysda <102615695+uiysda@users.noreply.github.com>
2023-06-04 04:28:35 +0800

    Update MUX_4to1.v

--- MUX_4to1.v
+++ MUX_4to1.v
@@ -3,13 +3,13 @@
 module MUX_4to1( Sel, DataIn0, DataIn1, DataIn2, DataIn3, DataOut );
   //四個進來選一個出去
 
-  input [31:0] DataIn0 ;
-  input [31:0] DataIn1 ;
-  input [31:0] DataIn2 ;
-  input [31:0] DataIn3 ;
+  input DataIn0 ;
+  input DataIn1 ;
+  input DataIn2 ;
+  input DataIn3 ;
   input [1:0] Sel ;
 
-  output [31:0] DataOut ;
+  output DataOut ;
 
   assign DataOut = Sel[1] ? ( Sel[0] ? DataIn3 : DataIn2 ) : ( Sel[0] ? DataIn1 : DataIn0 );
 
