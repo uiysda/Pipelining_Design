38eb24397ad45be4a0a7404df59ba1231d70f6de
David25062976 <102165811+David25062976@users.noreply.github.com>
2023-06-05 00:00:20 +0800

    Update MUX_4to1.v

--- MUX_4to1.v
+++ MUX_4to1.v
@@ -1,16 +1,17 @@
 `timescale 1ns/1ns
 
-module MUX_4to1( Sel, DataIn0, DataIn1, DataIn2, DataIn3, DataOut );
-  //四個進來選一個出去
+module MUX_4to1 #( parameter Width = 32 ) ( Sel, DataIn0, DataIn1, DataIn2, DataIn3, DataOut );
 
-  input DataIn0 ;
-  input DataIn1 ;
-  input DataIn2 ;
-  input DataIn3 ;
+  input [Width-1:0] DataIn0 ;
+  input [Width-1:0] DataIn1 ;
+  input [Width-1:0] DataIn2 ;
+  input [Width-1:0] DataIn3 ;
   input [1:0] Sel ;
 
-  output DataOut ;
+  output [Width-1:0] DataOut ;
 
   assign DataOut = Sel[1] ? ( Sel[0] ? DataIn3 : DataIn2 ) : ( Sel[0] ? DataIn1 : DataIn0 );
 
 endmodule
+
+
