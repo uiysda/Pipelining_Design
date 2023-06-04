2f07ab751a09740c29b17cf7cf1bedd9264e39c5
David25062976 <102165811+David25062976@users.noreply.github.com>
2023-06-04 23:59:57 +0800

    Update MUX_2to1.v

--- MUX_2to1.v
+++ MUX_2to1.v
@@ -1,11 +1,13 @@
 `timescale 1ns/1ns
 
-module MUX_2to1( Sel, DataIn0, DataIn1, DataOut );
+module MUX_2to1 #( parameter Width = 32 ) ( Sel, DataIn0, DataIn1, DataOut );
 
-	input Sel, DataIn0, DataIn1;
+	input Sel;
+	input [Width-1:0] DataIn0, DataIn1;
 
-	output DataOut;
+	output [Width-1:0] DataOut;
 
-	assign DataOut = ( Sel == 1'd0 ) ? DataIn0 : DataIn1;
+	assign DataOut = ( Sel == 1'b0 ) ? DataIn0 : DataIn1;
 
 endmodule
+
